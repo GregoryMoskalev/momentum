6193831e6b53fed2636bc3de8c25e968be4621b0
Gregory Moskalev <onberfrest@gmail.com>
2020-10-19 15:19:29 +0300

    feat: add basic setBgGreet function

--- momentum/main.js
+++ momentum/main.js
@@ -23,10 +23,37 @@ function showTime() {
   setTimeout(showTime, 1000);
 }
 
-// Add Zeros
+// Add zeros
 function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+// Set background and greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 6) {
+    //Night
+    document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = 'Good Night';
+    document.body.style.color = 'white';
+  } else if (hour < 12) {
+    //Morning
+    document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = 'Good Morning';
+  } else if (hour < 18) {
+    //Afternoon
+    document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon';
+  } else if (hour < 24) {
+    // Evening
+    document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = 'Good Evening';
+    document.body.style.color = 'white';
+  }
+}
+
 //Run
 showTime();
+setBgGreet();
